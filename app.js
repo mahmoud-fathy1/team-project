08d7bac809a8984e67f71b8c8c5e09ba602028a3
Mahmoud <mahmoudft8@gmail.com>
2025-07-21 14:27:01 +0300

    moddfied port number

--- app.js
+++ app.js
@@ -6,21 +6,71 @@ const cookieParser = require("cookie-parser");
 const { query, body, header, validationResult, matchedData } = require("express-validator");
 const User = require("./models/user.model.js");
 const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
+// const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const winston = require("winston");
+const { createClient } = require("redis");
+const { json } = require("stream/consumers");
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 2,
+const redisClient = createClient();
+
+redisClient
+    .connect()
+    .then(() => {
+        console.log("Connected to Redis server");
+    })
+    .catch((err) => {
+        logger.error(err);
+    });
+
+redisClient.on("error", (err) => {
+    logger.error(err);
 });
 
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+        }),
+        new winston.transports.File({ filename: "logs/app.log" }),
+    ],
+});
+
+// logger.info("info log");
+// logger.warn("warn log");
+// logger.error("error log");
+// logger.silly("error log");
+// logger.debug("error log");
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "logs", "access.log"), {
+    flags: "a",
+});
+
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 2,
+// });
+
 const app = express();
-app.use("/", limiter);
+// app.use("/", limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use("/posts", postRoutes);
-app.use("/users", userRoutes);
+app.use(
+    morgan("combined", {
+        stream: {
+            write: function (log) {
+                logger.info(log);
+            },
+        },
+    })
+);
+// app.use(morgan("dev"));
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
@@ -52,6 +102,21 @@ app.post(
     }
 );
 
+app.get("/data", async (req, res, next) => {
+    const cachedData = await redisClient.get("bigData");
+
+    if (cachedData) {
+        res.json(JSON.parse(cachedData));
+        return;
+    }
+    const response = await fetch("https://microsoftedge.github.io/Demos/json-dummy-data/5MB.json");
+    const data = await response.json();
+
+    redisClient.set("bigData", JSON.stringify(data));
+
+    res.json(data);
+});
+
 app.use((err, req, res, next) => {
     console.log(err);
 
@@ -64,3 +129,8 @@ app.use((err, req, res, next) => {
     });
 });
 module.exports = app;
+
+try {
+    const username = "Ali";
+    username = "Ahmed";
+} catch (error) {}
--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
