434b1c5f944752a0b9638593faa018be6a68d3c2
Mahmoud <mahmoudft8@gmail.com>
2025-07-23 13:56:42 +0300

    overwrite existing repo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const connectDB = require("./config/db");
 const express = require("express");
-const postRoutes = require("./routes/post.routes");
+// const postRoutes = require("./routes/post.routes");
 const userRoutes = require("./routes/user.routes.js");
 const cookieParser = require("cookie-parser");
 
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // app.use("/posts", postRoutes);
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the API");
