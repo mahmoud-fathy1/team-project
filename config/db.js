8db9d47190aa1b5cedf1b7ba91b8336d385b5754
Mahmoud <mahmoudft8@gmail.com>
2025-07-23 13:31:17 +0300

    overwrite existing repo

--- config/db.js
+++ config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"));
+    mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => console.log("MongoDB connected"))
+        .catch(err, () => {
+            console.log(err);
+        });
 };
 
 module.exports = connectDB;
